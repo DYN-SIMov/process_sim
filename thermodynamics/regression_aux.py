58d3eb54f5a7e45871f0feb993abdcb579cca7c4
maksimov-ps <maksimov.ps95@gmail.com>
2025-12-10 22:05:23 +0200

    working on BIP regression routine - linked SRK to Raoult's equation

--- thermodynamics/regression_aux.py
+++ thermodynamics/regression_aux.py
@@ -14,6 +14,10 @@ from termcolor import colored               # for colored text output
 from CoolProp.CoolProp import PropsSI
 from chemicals import CAS_from_any
 
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from properties import SoaveRedlichKwongEoSBackend, PureComponentDataBackend
 
 
 class ActivityModelInterface(Protocol):
@@ -53,24 +57,28 @@ class WilsonActivityModel():
 
     def objective_function(self, 
                            theta: np.ndarray,
-                           y_exp_val: float,
-                           x_exp_val: float,
-                           saturation_pressure_Pa_1: float,
-                           saturation_pressure_Pa_2: float,
-                           pressure_Pa: float) -> float:
+                           regression_params) -> float:
 
         " Objective function to minimize for Wilson BIP parameters estimation. "
 
         lambda_12 = theta[0]
         lambda_21 = theta[1]
 
+        x_exp_val = regression_params['x1_val']
+        y_exp_val = regression_params['y1_val']
+        saturation_pressure_Pa_1 = regression_params['saturation_pressure_Pa_1']
+        saturation_pressure_Pa_2 = regression_params['saturation_pressure_Pa_2']
+        pressure_Pa = regression_params['pressure_Pa']
+        fugacity_coef_1 = regression_params['fugacity_coefs'][0]
+        fugacity_coef_2 = regression_params['fugacity_coefs'][1]
+
         gamma_1_calc, gamma_2_calc = self.get_activity_coefs(lambda_12 = lambda_12,
                                                              lambda_21 = lambda_21,
                                                              x_val = x_exp_val)
 
-        # based on the modified Raoult's law: y_i * P_total= x_i * gamma_i * P_sat_i 
-        y_calc_val_1 = x_exp_val * gamma_1_calc * saturation_pressure_Pa_1 / pressure_Pa
-        y_calc_val_2 = (1 - x_exp_val) * gamma_2_calc * saturation_pressure_Pa_2 / pressure_Pa
+        # based on the modified Raoult's law: y_i * fi_i * P_total= x_i * gamma_i * P_sat_i 
+        y_calc_val_1 = x_exp_val * gamma_1_calc * saturation_pressure_Pa_1 / (pressure_Pa * fugacity_coef_1)
+        y_calc_val_2 = (1 - x_exp_val) * gamma_2_calc * saturation_pressure_Pa_2 / (pressure_Pa * fugacity_coef_2)
 
         y_exp_val_1  = y_exp_val
         y_exp_val_2  = 1 - y_exp_val
@@ -80,13 +88,39 @@ class WilsonActivityModel():
         return error
     
 
-    def initial_guess(self,
-                      initial_guess) -> np.ndarray:
+    @staticmethod
+    def initial_guess(initial_guess) -> np.ndarray:
         
         if initial_guess is None:
             return np.array([1.0, 1.0])
         else:
             return initial_guess
+        
+
+    @staticmethod
+    def get_message(regression_params: dict,
+                    components: list[str],
+                    result) -> str:
+        
+        temperature_K = regression_params['temperature_K']
+        pressure_atm  = regression_params['pressure_Pa'] / 1e5
+        x1_val = regression_params['x1_val']
+        y1_val = regression_params['y1_val']
+
+        if result.success:
+            msg = (f"T = {temperature_K:.2f} K, P = {pressure_atm:.2f} atm, "
+                   f"x_{components[0]} = {x1_val:.3f}, y_{components[0]} = {y1_val:.3f} --> "
+                   f" Fitted Wilson BIP parameters: "
+                   f" lambda_12 = {result.x[0]:.6f}, "
+                   f" lambda_21 = {result.x[1]:.6f}. "
+                   f" residual = {result.fun:.6e}.")
+        else:
+            msg = (f" BIP parameters regression did not converge: "
+                   f" {result.message} ")
+
+        return msg
+
+
 
 
 class NRTLActivityModel():
@@ -95,24 +129,15 @@ class NRTLActivityModel():
         pass
 
 
+
 class BinaryInteractionParametersRegression(): 
 
     def __init__(self,
-                 activity_model: str = None) -> None: 
+                 activity_model: str = None,
+                 equation_of_state: str = None) -> None: 
 
-        if activity_model.upper() == 'WILSON':
-            self.activity_model_backend = WilsonActivityModel()
-        elif activity_model.upper() == 'NRTL':
-            self.activity_model_backend = NRTLActivityModel()
-        else:
-            raise ValueError(" Unsupported activity model specified for BIP parameters regression. ")
-
-
-        if activity_model is None: 
-            self.activity_model_backend = WilsonActivityModel()
-            warning_msg = " No activity model specified for BIP parameters regression. " \
-                          " Wilson activity model will be used by default. " 
-            print(colored(warning_msg, 'yellow'))
+        self.activity_model = activity_model
+        self.equation_of_state = equation_of_state
 
 
     def _parse_components_from_comment(self, filepath: str) -> list[str]:
@@ -185,7 +210,6 @@ class BinaryInteractionParametersRegression():
             msg =(f" Data import: "
                   f" Detected isobaric VLE data set for {self.components[0]} and {self.components[1]} " 
                   f"at P = {pressure_Pa[0]/1e5:.2f} bar. ")
-            print(msg)
 
         elif is_constant(temperature_K):
             # Isothermal data set
@@ -201,7 +225,6 @@ class BinaryInteractionParametersRegression():
             msg =(f" Data import: "
                   f" Detected isothermal VLE data set for {self.components[0]} and {self.components[1]} " 
                   f" at T = {temperature_K[0]:.2f} K. ")
-            print(msg)
 
         else:
 
@@ -225,7 +248,6 @@ class BinaryInteractionParametersRegression():
                 msg =(f" Data import: "
                       f" Detected mixed VLE data set for {self.components[0]} and {self.components[1]}. "
                       f" Split into {len(unique_pressures)} isobaric subsets. ")
-                print(msg)
 
             else:
                 # More unique pressures - treat as isothermal sets
@@ -243,9 +265,50 @@ class BinaryInteractionParametersRegression():
                 msg = (f" Data import: "
                        f" Detected mixed VLE data set for {self.components[0]} and {self.components[1]}. " 
                        f" Split into {len(unique_temperatures)} isothermal subsets. ")
-                print(msg)
+
+        if msg:        
+            print(colored(msg, 'green'))
+        else:
+            raise print(colored(" Data import: Unable to detect VLE data type (isobaric/isothermal/mixed). ",'red'))
+        
+
+    def _configure_activity_model_backend(self,
+                                          components: list) -> None:
+
+        if self.activity_model is not None:
+            if self.activity_model.upper() == 'WILSON':
+                self.activity_model_backend = WilsonActivityModel()
+            elif self.activity_model.upper() == 'NRTL':
+                self.activity_model_backend = NRTLActivityModel()
+            else:
+                raise ValueError(" Unsupported activity model specified for BIP parameters regression. ")
+            
+        else: 
+            self.activity_model_backend = WilsonActivityModel()
+            warning_msg = " No activity model specified for BIP parameters regression. " \
+                          " Wilson activity model will be used by default. " 
+            print(colored(warning_msg, 'yellow'))
 
         pass
+
+
+    def _configure_eos_backend(self,
+                               components: list) -> None:
+
+        if self.equation_of_state is not None:
+            if self.equation_of_state.upper() in ['SRK', 'SOAVE-REDLICH-KWONG', 'SOAVE REDLICH KWONG']:
+                pure_component_data_backend = PureComponentDataBackend(components = components)
+                self.eos_backend = SoaveRedlichKwongEoSBackend(components=components,
+                                                               pure_component_data_backend=pure_component_data_backend)
+
+                pass
+
+            elif self.equation_of_state.upper() in ['PR', 'PENG-ROBERTSON', 'PENG ROBERTSON']:
+                raise NotImplementedError(" Peng-Robertson EoS backend for BIP parameters regression is not implemented yet. ")
+            else:
+                raise ValueError(" Unsupported equation of state specified for BIP parameters regression. ")
+
+        pass 
 
 
     def data_import(self, filepath: str) -> None:
@@ -261,40 +324,73 @@ class BinaryInteractionParametersRegression():
         data = self._extract_data(dataframe = df, components = self.components)
 
         self._detect_data(data = data)
+        self._configure_activity_model_backend(components=components)
+        self._configure_eos_backend(components=components)
 
         pass
 
     
     
-    def regress_BIP_parameters(self) -> None:
+    def regress_BIP_parameters_elementwise(self) -> None:
         
-        opt_results = []
+        opt_data_results = []
+        opt_data_temperature_K = []
+        opt_data_pressure_Pa = []
 
         for data_set in self.data_set:
             for k in range(len(data_set['x_data'])):
 
-                saturation_pressure_Pa_1 = PropsSI('P','T',data_set['temperature_K'][k],'Q',1,CAS_from_any(self.components[0]))
-                saturation_pressure_Pa_2 = PropsSI('P','T',data_set['temperature_K'][k],'Q',1,CAS_from_any(self.components[1]))
+                try: 
+                    saturation_pressure_Pa_1 = PropsSI('P','T',data_set['temperature_K'][k],'Q',1,CAS_from_any(self.components[0]))
+                    saturation_pressure_Pa_2 = PropsSI('P','T',data_set['temperature_K'][k],'Q',1,CAS_from_any(self.components[1]))
+                except:
+                    msg = (f" Saturation pressure calculation failed for T = {data_set['temperature_K'][k]:.2f} K. "
+                           f" Skipping this data point." 
+                           f" Check if critical temperature is exceeded or if components are not supported by CoolProp. ")
+                    print(colored(msg, 'yellow'))
+                    break
 
-                args = (data_set['y_data'][k],
-                        data_set['x_data'][k],
-                        saturation_pressure_Pa_1,
-                        saturation_pressure_Pa_2,
-                        data_set['pressure_Pa'][k])
+
+                if self.equation_of_state is not None:
+                    fugacity_coefs    = self.eos_backend.get_fugacity_coefs(temperature_K = data_set['temperature_K'][k],
+                                                                            pressure_Pa = data_set['pressure_Pa'][k],
+                                                                            molar_composition = np.array([data_set['y_data'][k], 1 - data_set['y_data'][k]])) 
+                else: 
+                    fugacity_coefs    = np.array([1.0, 1.0])   # ideal gas assumption if no EoS backend is specified
+
+
+                regression_params = {'y1_val': data_set['y_data'][k],
+                                     'x1_val': data_set['x_data'][k],
+                                     'saturation_pressure_Pa_1': saturation_pressure_Pa_1,
+                                     'saturation_pressure_Pa_2': saturation_pressure_Pa_2,
+                                     'pressure_Pa': data_set['pressure_Pa'][k],
+                                     'temperature_K': data_set['temperature_K'][k],
+                                     'fugacity_coefs': fugacity_coefs}
 
                 result = minimize(fun = self.activity_model_backend.objective_function,
-                                  x0 = self.activity_model_backend.initial_guess(initial_guess=opt_results[-1] 
-                                                                                 if len(opt_results) > 0 else None),
-                                  args = args,
+                                  x0 = self.activity_model_backend.initial_guess(initial_guess=opt_data_results[-1] 
+                                                                                 if len(opt_data_results) > 0 else None),
+                                  args = (regression_params),
                                   method = 'SLSQP',
                                   bounds=((1e-3, None), (1e-3, None)))
 
-                opt_results.append(result.x)
+                msg = self.activity_model_backend.get_message(result=result,
+                                                              regression_params=regression_params,
+                                                              components=self.components)
+                print(msg)
 
-                pass
-        
+                opt_data_results.append(result.x)
+                opt_data_temperature_K.append(data_set['temperature_K'][k])
+                opt_data_pressure_Pa.append(data_set['pressure_Pa'][k])
+                
+            print()  # blank line between data sets
+
+        self.opt_results = np.array(opt_data_results)
+        self.opt_temperature_K = np.array(opt_data_temperature_K)
+        self.opt_pressure_Pa = np.array(opt_data_pressure_Pa)
 
         pass
+
 
 
 
--- thermodynamics/regression_main.py
+++ thermodynamics/regression_main.py
@@ -4,8 +4,9 @@ from regression_aux import WilsonActivityModel
 
 if __name__ == '__main__': 
 
-    wilson_BIP_estimator = BinaryInteractionParametersRegression(activity_model='wilson')
+    wilson_BIP_estimator = BinaryInteractionParametersRegression(activity_model='wilson',
+                                                                 equation_of_state=None,)
     wilson_BIP_estimator.data_import(filepath = 'thermodynamics/thermo_data/VLE_H2O_NH3.csv')
-    wilson_BIP_estimator.regress_BIP_parameters()
+    wilson_BIP_estimator.regress_BIP_parameters_elementwise()
 
     pass
